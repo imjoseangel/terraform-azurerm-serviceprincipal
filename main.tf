e2ad64861e300a0489c9f6cbd0c220a7abf3b6be
imjoseangel <josea.munoz@gmail.com>
2021-12-16 20:01:26 +0100

    add owner and roles

--- main.tf
+++ main.tf
@@ -6,19 +6,23 @@ data "azuread_client_config" "main" {}
 
 data "azurerm_subscription" "main" {}
 
+locals {
+  scopes = length(var.scopes) > 0 ? var.scopes : [data.azurerm_subscription.main.id]
+}
+
 #---------------------------------------------------------
 # Service Principal Creation or selection
 #----------------------------------------------------------
 
 resource "azuread_application" "main" {
   display_name = lower(var.name)
-  owners       = [data.azuread_client_config.main.object_id]
+  owners       = compact([data.azuread_client_config.main.object_id, var.owner_object_id])
 }
 
 resource "azuread_service_principal" "main" {
   application_id               = azuread_application.main.application_id
   app_role_assignment_required = true
-  owners                       = [data.azuread_client_config.main.object_id]
+  owners                       = compact([data.azuread_client_config.main.object_id, var.owner_object_id])
 }
 
 resource "time_rotating" "main" {
@@ -30,4 +34,16 @@ resource "azuread_service_principal_password" "main" {
   rotate_when_changed = {
     rotation = time_rotating.main.id
   }
+}
+
+data "azurerm_role_definition" "main" {
+  count = var.role != "" ? 1 : 0
+  name  = var.role
+}
+
+resource "azurerm_role_assignment" "main" {
+  count              = var.role != "" ? length(local.scopes) : 0
+  scope              = local.scopes[count.index]
+  role_definition_id = format("%s%s", data.azurerm_subscription.main.id, data.azurerm_role_definition.main[0].id)
+  principal_id       = azuread_service_principal.main.id
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,21 @@ variable "name" {
   type        = string
   description = "The name of the service principal."
 }
+
+variable "owner_object_id" {
+  type        = string
+  description = "The owner object id of the service principal."
+  default     = ""
+}
+
+variable "role" {
+  type        = string
+  default     = ""
+  description = "The name of a role for the service principal."
+}
+
+variable "scopes" {
+  type        = list(string)
+  default     = []
+  description = "A list of scopes the role assignment applies to."
+}
