b11c4ce9dac6060f9085a6b78a0d74fa4fe600fd
imjoseangel <josea.munoz@gmail.com>
2021-12-16 17:15:42 +0100

    add password

--- main.tf
+++ main.tf
@@ -20,3 +20,14 @@ resource "azuread_service_principal" "main" {
   app_role_assignment_required = true
   owners                       = [data.azuread_client_config.main.object_id]
 }
+
+resource "time_rotating" "main" {
+  rotation_days = 730
+}
+
+resource "azuread_service_principal_password" "main" {
+  service_principal_id = azuread_service_principal.main.id
+  rotate_when_changed = {
+    rotation = time_rotating.main.id
+  }
+}
