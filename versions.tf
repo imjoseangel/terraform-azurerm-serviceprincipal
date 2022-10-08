de20f91cbc135dd991fc709a14c254dc23dff84b
imjoseangel <josea.munoz@gmail.com>
2022-10-08 17:27:51 +0200

    add time

--- versions.tf
+++ versions.tf
@@ -2,11 +2,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.25.0"
+      version = "~> 3.26.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
       version = "~> 2.29.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.8.0"
     }
   }
   required_version = ">= 1.0.0"
